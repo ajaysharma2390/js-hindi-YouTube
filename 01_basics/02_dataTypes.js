239f7aaeb2355517f88ca0ba54d1f80fd7124938
ajaysharma2390 <111680761+ajaysharma2390@users.noreply.github.com>
2026-04-14 12:22:44 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+//  node 01_basics/02_dataTypes.js
+
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3
+     + 
+     3) // Code Readability should be high
+
+console.log("Hitesh")
+
+
+let name = "Ajay"
+let age = 24
+let isLoggedIn = false
+let state;
+
+//number range => 2 to power 53
+//bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+//objects
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+console.log({} == {}); // false --> bcoz it is comparing memory locations not the two objects
