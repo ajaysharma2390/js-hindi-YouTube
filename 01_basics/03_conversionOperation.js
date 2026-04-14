86529dbd4955eec9005142c2fa5d8ecbbc651851
ajaysharma2390 <111680761+ajaysharma2390@users.noreply.github.com>
2026-04-14 19:40:59 +0000

    Conversion Of Datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,35 @@
-let score = 33
+let score = "invincible"
 
-console.log(typeof score)
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN & the type is still => number
+// true => 1; false => 0
+// "Ajay" => NaN
+// null => 0
+// undefined => NaN & the type is still => undefined   
+
+
+
+
+let isLoggedIn = "Ajay"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Ajay" => true  
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
