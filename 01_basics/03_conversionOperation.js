5bb923f16a5129be7094523cd239788418cfcfda
Ajay Sharma <111680761+ajaysharma2390@users.noreply.github.com>
2026-04-15 10:52:26 +0000

    Operation & prefix/postfix incremental operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,6 @@
+// ********************* Conversion **********************
+// link to study: https://tc39.es/ecma262/multipage/abstract-operations.html
+
 let score = "invincible"
 
 // console.log(typeof score);
@@ -31,5 +34,53 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************************** Operations *****************************
+
+let value = 3
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Ajay"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2" + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+// console.log(gameCounter);
+
+let a = 3
+let b = a++
+console.log(`a:${a}, b:${b}`);
+//y gets 3 (=> x) then x becomes 4
+
+let x = 3
+let y = ++x
+console.log(`x:${x}, y:${y}`);
+//x becomes 4 (=>incremented x) then y gets 4
+// link to study pre/post increment: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
