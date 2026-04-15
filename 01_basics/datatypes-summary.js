be83b5564b1abb48a99f03b693bca510963f1336
Ajay Sharma <111680761+ajaysharma2390@users.noreply.github.com>
2026-04-15 12:33:17 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,43 @@
+// Primitive Datatype:
+
+// 7 Types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 3423555223835327474n
+
+
+
+
+//Non Primitive/ Reference Datatype:
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "ironman", "ninja hattori"];
+
+let myObj = {
+    name: "Ajay",
+    age: 24,
+    role: "SDE",
+}
+
+const myFunction = function() {
+    console.log("Hello World");
+}
+
+console.log(typeof bigInt);
+console.log(typeof myFunction);
+console.log(typeof heros);
+console.log(typeof outsideTemp);
+console.log(typeof id);
+console.log(typeof userEmail);
