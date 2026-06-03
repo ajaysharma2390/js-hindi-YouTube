102d585500fb5ee0582ea85801f88789378f17fc
Ajay Sharma <111680761+ajaysharma2390@users.noreply.github.com>
2026-06-03 21:19:06 +0000

    getOwnPropertyDescriptor and defineProperty to set property flag

---
+++ 10_classes_and_oop/mathpi.js
@@ -0,0 +1,38 @@
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
+
+// console.log(descripter);
+
+// console.log(Math.PI);
+// Math.PI = 5
+// console.log(Math.PI);
+
+
+
+// ++++++++++++++++++++++++++ can we give the property flags (like writable, enumerable, configurable) to our object => YES, so lets try ++++++++++++++++++++++++++++
+
+const chai = {
+    name: 'ginger chai',
+    price: 250,
+    isAvailable: true,
+
+    orderChai: function(){
+        console.log("chai nhi bni");
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+Object.defineProperty(chai, 'name', {
+    //writable: false,
+    enumerable: true,
+    
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (let [key, value] of Object.entries(chai)) {
+    if (typeof value !== 'function') {
+        
+        console.log(`${key} : ${value}`);
+    }
+}
